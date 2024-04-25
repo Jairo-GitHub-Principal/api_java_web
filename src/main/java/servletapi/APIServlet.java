54c2c4f93235216effa914176fbefccd5c5d7716
Jairo-GitHub-Principal <jairocesa@outlook.com.br>
2024-04-25 00:49:38 -0300

    aplicação java web, com uma api intermediaria que consome uma api rest, e exibe o conteudo da mesma em uma pagina web

---
+++ src/main/java/servletapi/APIServlet.java
@@ -0,0 +1,59 @@
+package servletapi;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import testeweb2.APIClient;
+
+import java.io.IOException;
+import javax.servlet.*;
+
+/**
+ * Servlet implementation class APIServlet
+ */
+public class APIServlet extends HttpServlet implements Servlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public APIServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * 
+	 * http://makeup-api.herokuapp.com/api/v1/products.json
+
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+		
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Aqui você irá implementar a lógica para obter os dados da API e enviar como resposta
+        String apiUrl = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"; // URL da sua API
+        
+        String responseData = APIClient.fetchDataFromAPI(apiUrl); // Utilize a classe APIClient para buscar os dados
+       
+//        
+//        RequestDispatcher dispatcher = request.getRequestDispatcher("/index.html");
+//        dispatcher.forward(request, response);
+        
+        
+        response.setContentType("application/json");
+        response.getWriter().write(responseData);
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
